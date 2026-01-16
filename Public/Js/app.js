a9b092dd617a50bcad4f880aedb955de5c5c7d87
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2026-01-16 20:27:27 +0100

    added the function change icon in the container title

--- Public/Js/app.js
+++ Public/Js/app.js
@@ -1,13 +1,26 @@
 const square = document.querySelectorAll('.element');
 const btn = document.querySelector('button');
+const secondX = document.querySelector(
+    '.title div:nth-child(2) .bi-x'
+);
+
 
 let statuss = false;
+
+
+let changeIcon = () => {
+    !statuss
+        ? secondX.setAttribute('class', 'bi bi-circle')
+        : secondX.setAttribute('class', 'bi bi-x')
+
+}
 
 let addedElement = (v) => {
     v.innerHTML = statuss
         ? '<i class="bi bi-x"></i>'
         : '<i class="bi bi-circle"></i>';
     statuss = !statuss;
+    changeIcon();
 }
 
 btn.addEventListener('click', () => {
