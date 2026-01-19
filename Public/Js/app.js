5d5a9d66c383082779bf780b115f4016d679af20
Anas-elbourkkadi <elbourkkadianas@gmail.com>
2026-01-19 10:02:27 +0100

    added the function check winer

--- Public/Js/app.js
+++ Public/Js/app.js
@@ -3,10 +3,23 @@ const btn = document.querySelector('button');
 const secondX = document.querySelector(
     '.title div:nth-child(2) .bi-x'
 );
+const listArchife =
+    [
+        [square[0], square[1], square[2]],
+        [square[3], square[4], square[5]],
+        [square[6], square[7], square[8]],
+
+        [square[0], square[3], square[6]],
+        [square[1], square[4], square[7]],
+        [square[2], square[5], square[8]],
+
+        [square[0], square[4], square[8]],
+        [square[2], square[4], square[6]],
+    ]
+
 
 
 let statuss = false;
-
 
 let changeIcon = () => {
     !statuss
@@ -20,7 +33,21 @@ let addedElement = (v) => {
         ? '<i class="bi bi-x"></i>'
         : '<i class="bi bi-circle"></i>';
     statuss = !statuss;
-    changeIcon();
+}
+
+let check = () => {
+    if (
+        listArchife.some(row =>
+            row.every(cell => cell.innerHTML === '<i class="bi bi-x"></i>')
+        )
+        ||
+        listArchife.some(row =>
+            row.every(cell => cell.innerHTML === '<i class="bi bi-x"></i>')
+        )
+    ) {
+        return true;
+    }
+
 }
 
 btn.addEventListener('click', () => {
@@ -28,12 +55,24 @@ btn.addEventListener('click', () => {
 })
 
 
-
 square.forEach((e) => {
     e.addEventListener('click', () => {
-        if (!e.innerHTML) addedElement(e);
+
+        if (e.innerHTML) return;
+
+        addedElement(e);
+        changeIcon();
+
+        let guess = check();
+
+        if (guess) {
+            console.log('you win 🎉');
+            return;
+        }
 
 
-    })
-
-})
+        if ([...square].every(s => s.innerHTML)) {
+            console.log('draw 😐');
+        }
+    });
+});
